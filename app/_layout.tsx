3fb9a6a88ba4a8c75abd3b73cb421ba842810cbe
feijaoozin <gonardo1234@gmail.com>
2026-06-05 12:36:10 -0300

    feat: configurando banco sqlite e migrações iniciais

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,16 @@
 import { Slot } from "expo-router";
+import { useEffect } from "react";
 
 import Toast from "react-native-toast-message";
 
 import { ThemeProvider } from "@/src/contexts/ThemeContext";
 import { initDB } from "@/src/database/migrations";
-import { useEffect } from "react";
 
 export default function RootLayout() {
   useEffect(() => {
     initDB();
   }, []);
+
   return (
     <ThemeProvider>
       <Slot />
