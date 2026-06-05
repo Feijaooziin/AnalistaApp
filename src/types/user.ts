33b68024b9d9c005b3ab5e6d76e9f0caf1dae49e
feijaoozin <gonardo1234@gmail.com>
2026-06-05 12:37:52 -0300

    feat: iniciando módulo de gestão de funcionários da jbs

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export type User = {
+  id?: number;
+  nome: string;
+  matricula?: string;
+  cargo?: string;
+  escala?: string;
+  endereco?: string;
+  email?: string;
+  telefone?: string;
+  created_at?: string;
+};
