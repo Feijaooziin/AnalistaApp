8f066d8fbedd4674b502b9535ce2767a186a632c
feijaoozin <gonardo1234@gmail.com>
2026-06-05 12:39:46 -0300

    feat: adicionando repository de funcionários da jbs

--- src/database/repositories/usersJbsRepository.ts
+++ src/database/repositories/usersJbsRepository.ts
@@ -1,15 +1,90 @@
+import { User } from "@/src/types/user";
 import { db } from "../db";
 
 export const usersJbsRepository = {
   async list() {
-    return await db.getAllAsync("SELECT * FROM usersJbs ORDER BY nome ASC");
+    return await db.getAllAsync<User>(
+      `
+      SELECT *
+      FROM usersJbs
+      ORDER BY nome ASC
+      `,
+    );
   },
 
   async findById(id: number) {
-    return await db.getFirstAsync("SELECT * FROM usersJbs WHERE id = ?", [id]);
+    return await db.getFirstAsync<User>(
+      `
+      SELECT *
+      FROM usersJbs
+      WHERE id = ?
+      `,
+      [id],
+    );
+  },
+
+  async create(user: User) {
+    await db.runAsync(
+      `
+      INSERT INTO usersJbs (
+        nome,
+        matricula,
+        cargo,
+        escala,
+        endereco,
+        email,
+        telefone,
+        created_at
+      )
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+      `,
+      [
+        user.nome,
+        user.matricula ?? null,
+        user.cargo ?? null,
+        user.escala ?? null,
+        user.endereco ?? null,
+        user.email ?? null,
+        user.telefone ?? null,
+        new Date().toISOString(),
+      ],
+    );
+  },
+
+  async update(user: User) {
+    await db.runAsync(
+      `
+      UPDATE usersJbs
+      SET
+        nome = ?,
+        matricula = ?,
+        cargo = ?,
+        escala = ?,
+        endereco = ?,
+        email = ?,
+        telefone = ?
+      WHERE id = ?
+      `,
+      [
+        user.nome,
+        user.matricula ?? null,
+        user.cargo ?? null,
+        user.escala ?? null,
+        user.endereco ?? null,
+        user.email ?? null,
+        user.telefone ?? null,
+        user.id,
+      ],
+    );
   },
 
   async remove(id: number) {
-    await db.runAsync("DELETE FROM usersJbs WHERE id = ?", [id]);
+    await db.runAsync(
+      `
+      DELETE FROM usersJbs
+      WHERE id = ?
+      `,
+      [id],
+    );
   },
 };
